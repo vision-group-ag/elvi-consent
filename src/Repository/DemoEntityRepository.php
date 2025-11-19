d5276b1b316faa7289bb3f8964b503bc3e2b194b
Saulius <saulius.cepauskas@visiongroup.io>
2025-11-19 15:48:47 +0200

    Libs update - doctrine-bundle - ecs

--- src/Repository/DemoEntityRepository.php
+++ src/Repository/DemoEntityRepository.php
@@ -8,6 +8,7 @@ use App\Entity\DemoEntity;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+/** @extends ServiceEntityRepository<DemoEntity> */
 class DemoEntityRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
